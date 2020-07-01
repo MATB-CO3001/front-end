2677f76704743678dda73dbdb794dcb2db62aa8e
Nguyen Phuc <phuc.nguyenblackjack@hcmut.edu.vn>
2020-07-01 10:25:04 +0700

    complete the UI

---
+++ footer.php
@@ -0,0 +1,84 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+</head>
+
+<body>
+	<div id="footer" class="w3-container w3-border-bottom">
+		<div class="w3-row">
+			<div id="left" class="w3-quarter w3-padding">
+				<h6 class="w3-padding">HOTLINE</h6>
+                 <div class="footer-about">
+                     <ul class="w3-ul">
+                     	<li><i class="fa fa-phone"></i>&ensp;(84-8) 38647256 - 5200</li>
+                        <li><i class="far fa-envelope"></i>&ensp; bksfcs@hcmut.edu.vn</li>
+                     </ul>
+                 </div>
+				<h6 class="w3-padding">VĂN PHÒNG</h6>
+                 <div class="footer-about">
+                     <ul class="w3-ul">
+                     	<li><i class="fas fa-map-marker-alt"></i>&ensp; 268 Lý Thường Kiệt,Phường 14,Quận 10, TP.HCM</li>
+                     </ul>
+                 </div>
+			</div>
+			<div id="left-middle" class="w3-quarter w3-padding">
+				<h6 class="w3-padding">HỖ TRỢ KHÁCH HÀNG</h6>
+                 <div class="footer-about">
+                     <ul class="w3-ul">
+                     	<li>
+							<a href="#">Các câu hỏi thường gặp</a>
+						</li>
+						<li>
+							<a href="#">Hướng dẫn đặt hàng</a> 
+						</li>
+						<li>
+							<a href="#">Phương thức thanh toán</a> 
+						</li>
+                     </ul>
+                 </div>
+			</div>
+			<div id="right-middle" class="w3-quarter w3-padding" style="text-align: center">
+				<h6 class="w3-padding">PHƯƠNG THỨC THANH TOÁN</h6>
+                <div class="payment-method">
+                	<div class="pay-row">
+                    	<img class="icon-payment" src="./resources/image/mastercard.svg" width="54"
+                  				alt="">
+                        <img class="icon-payment" src="./resources/image/jcb.svg" width="54" alt="">
+                        <img class="icon-payment" src="./resources/image/visa.svg" width="54" alt="">
+                    </div>
+                    <div class="pay-row">
+                        <img class="icon-payment" src="./resources/image/cash.svg" width="54" alt="">
+                        <img class="icon-payment" src="./resources/image/internet-banking.svg"
+                                   width="54" alt="">
+                        <img class="icon-payment" src="./resources/image/installment.svg" width="54"
+                                   alt="">
+                    </div>
+                 </div>   
+			</div>
+			<div id="right" class="w3-quarter w3-padding" style="text-align: center">
+				<h6 class="w3-padding">KẾT NỐI VỚI CHÚNG TÔI</h6>
+				<div class="connect-us">
+                           <img class="icon-connect" src="./resources/image/021-facebook.svg" width="30"
+                               alt="Facebook">
+                           <img class="icon-connect" src="./resources/image/045-linkedin.svg" width="30"
+                               alt="Linkedin">
+                           <img class="icon-connect" src="./resources/image/025-instagram.svg" width="30"
+                               alt="Instagram">
+                           <img class="icon-connect" src="./resources/image/011-youtube.svg" width="35"
+                               alt="Youtube">
+                       </div>
+			</div>
+		</div>
+	</div>
+	 <div class="footer-bottom">
+       <div style="text-align: center">
+           <div>
+               <div>© 2020 – BKSFCS</div>
+               <span>Đại Học Bách Khoa Thành phố Hồ Chí Minh</span>
+           </div>
+       </div>
+   </div>
+</body>
+</html>
