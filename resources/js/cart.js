64cd7eecf66f4d973d99761c61fabd4a74b0459c
Ngoc Nguyen Minh <alanke01636253662@gmail.com>
2020-07-08 20:38:49 +0700

    add a bunch of refactored code

--- resources/js/cart.js
+++ resources/js/cart.js
@@ -1,25 +1,133 @@
 var navHeight = document.getElementById('nav-bar').clientHeight;
 
-$(document).ready(function() {
-    $('.purchase-btn').each(function(){ 
-        $(this).css({
-            'margin-bottom' : navHeight + 'px' 
-        });
-    });
-});
-
-const cart = document.getElementById('cart-container');
+// $(document).ready(function() {
+//     $('.purchase-btn').each(function(){ 
+//         $(this).css({
+//             'margin-bottom' : navHeight + 'px' 
+//         });
+//     });
+// });
 
 if (document.readyState == 'loading') {
-    document.addEventListener('DOMContentLoaded', ready)
+    document.addEventListener('DOMContentLoaded', ready);
 } else {
-    ready()
+    ready();
 }
 
 function ready() {
-    var removeCartItemButtons = document.getElementsByClassName('btn-danger')
+    var removeCartItemButtons = document.getElementsByClassName('btn-danger');
     for (var i = 0; i < removeCartItemButtons.length; i++) {
-        var button = removeCartItemButtons[i]
-        button.addEventListener('click', removeCartItem)
+        var button = removeCartItemButtons[i];
+        button.addEventListener('click', removeCartItem);
     }
+
+    var quantityInputs = document.getElementsByClassName('cart-quantity-input');
+    for (var i = 0; i < quantityInputs.length; i++) {
+        var input = quantityInputs[i]
+        input.addEventListener('change', quantityChanged);
+    }
+
+    var addToCartButtons = document.getElementsByClassName('modify-btn');
+    for (var i = 0; i < addToCartButtons.length; i++) {
+        var button = addToCartButtons[i];
+        button.addEventListener('click', addToCartClicked);
+    }
+
+    document.getElementsByClassName('purchase-btn')[0].addEventListener('click', purchaseClicked);
 }
+
+function purchaseClicked() {
+    // $(document).ready(function(){
+    //     let btn = document.getElementsByClassName('purchase-btn');
+    //     ok.addEventListener('click', () => {
+    //         $.post("demo_test_post.asp",
+    //             {
+    //                 foodName: 'Cơm canh rồng ',
+    //                 foodDesc: 'Cá trứng muối tiêu',
+    //                 price: 20000,
+    //                 image: 'resources/image/2.jpg'
+    //             },
+    //             function(status){
+    //                 alert("\nStatus: " + status);
+    //         });
+    //     });
+    // });
+    alert('Thank you for your purchase');
+    var cartItems = document.getElementById('cart-items');
+    while (cartItems.hasChildNodes()) {
+        cartItems.removeChild(cartItems.firstChild);
+    }
+    updateCartTotal();
+}
+
+function removeCartItem(event) {
+    var buttonClicked = event.target;
+    buttonClicked.parentElement.parentElement.remove();
+    updateCartTotal();
+}
+
+function quantityChanged(event) {
+    var input = event.target;
+    if (isNaN(input.value) || input.value <= 0) {
+        input.value = 1;
+    }
+    updateCartTotal();
+}
+
+function addToCartClicked(event) {
+    var button = event.target;
+    var shopItem = button.parentElement.parentElement;
+    var title = shopItem.getElementsByClassName('card-title')[0].innerText;
+    var price = shopItem.getElementsByClassName('card-price')[0].innerText;
+    var imageSrc = shopItem.getElementsByClassName('card-img-top')[0].src;
+    addItemToCart(title, price, imageSrc);
+    updateCartTotal();
+}
+
+function addItemToCart(title, price, imageSrc) {
+    var cartRow = document.createElement('div');
+    cartRow.classList.add('cart-row');
+    var cartItems = document.getElementById('cart-items');
+    var cartItemNames = cartItems.getElementsByClassName('cart-item-title');
+    for (var i = 0; i < cartItemNames.length; i++) {
+        if (cartItemNames[i].innerText == title) {
+            alert('Bạn đẫ thêm món này vào giỏ');
+            return;
+        }
+    }
+    var cartRowContents = `
+        <div class="cart-item cart-column">
+            <img class="cart-item-image" src="${imageSrc}" width="100" height="100">
+            <span class="cart-item-title">${title}</span>
+        </div>
+        <span class="cart-price cart-column">${price}</span>
+        <div class="cart-quantity cart-column">
+            <input class="cart-quantity-input" type="number" value="1">
+            <button class="btn btn-danger" type="button">Xoá</button>
+        </div>`;
+    cartRow.innerHTML = cartRowContents;
+    cartItems.append(cartRow);
+    cartRow.getElementsByClassName('btn-danger')[0].addEventListener('click', removeCartItem);
+    cartRow.getElementsByClassName('cart-quantity-input')[0].addEventListener('change', quantityChanged);
+}
+
+function updateCartTotal() {
+    var cartItemContainer = document.getElementById('cart-items');
+    var cartRows = cartItemContainer.getElementsByClassName('cart-row');
+    var total = 0;
+    for (var i = 0; i < cartRows.length; i++) {
+        var cartRow = cartRows[i];
+        var priceElement = cartRow.getElementsByClassName('cart-price')[0];
+        var quantityElement = cartRow.getElementsByClassName('cart-quantity-input')[0];
+
+        var price = priceElement.innerText;
+        var price = parseFloat(priceElement.innerText.replace(' VND', ''));
+        price = parseFloat(priceElement.innerText.replace(',', ''));
+
+        var quantity = quantityElement.value
+        total = total + (price * quantity);
+    }
+    total = Math.round(total * 100) / 100
+    document.getElementsByClassName('cart-total-price')[0].innerText = formatNumber(total) + ' VND';
+}
+
--- resources/js/foodsList.js
+++ resources/js/foodsList.js
@@ -1,34 +1,56 @@
-function createFoodinCourt(img, foodName, foodDesc, price) {
-    return [
-        '<div class="card">',
-            '<div class="img-dir">',
-                '<img class="card-img-top" src="' + img + '">',
-            '</div>',
-            '<div class="card-body">',
-                '<h5 class="card-title">' + foodName + '</h5>',
-                '<p class="card-text">' + foodDesc + '</p>',
-                '<a class="btn btn-primary modify-btn">' + price + '</a>',
-            '</div>',
-        '</div>'
-    ].join('\n');
+//const { each } = require("jquery");
+
+function createFoodinCourt(wrapper, item) {
+    var cardContent = `
+    <div class="card">
+        <div class="img-dir">
+            <img class="card-img-top" src="${item.image}">
+        </div>
+        <div class="card-body">
+            <h5 class="card-title">${item.name}</h5>
+            <p class="card-price">${formatNumber(item.price)} VND</p>
+            <a class="btn btn-primary modify-btn">Thêm vào giỏ</a>
+        </div>
+    </div>`;
+
+    wrapper.innerHTML += cardContent;
 }
 
 const container = document.getElementById('foods-container');
 
-for (let k = 0; k < NUM_OF_COURTS; k++) {
-    const courtFoodList = document.createElement('div');
-    courtFoodList.setAttribute('class', 'food-list');
-    courtFoodList.setAttribute('id', k);
-    courtFoodList.innerHTML += 
-        '<h3>' + courtInfo.name + '</h3>';
-    for (let i = 0; i < NUM_OF_FOODS_EACH_COURT["COURT" + k]; i++) {
-        let card = createFoodinCourt(
-            foodInfo[k].image, 
-            foodInfo[k].foodName, 
-            foodInfo[k].foodDesc,
-            foodInfo[k].price
-        );
-        courtFoodList.innerHTML += card;
+// for (let k = 0; k < NUM_OF_COURTS; k++) {
+//     const courtFoodList = document.createElement('div');
+//     courtFoodList.classList.add('food-list');
+//     courtFoodList.setAttribute('id', k);
+
+//     courtFoodList.innerHTML += `<h3>${courtInfo.name}</h3>`;
+
+//     for (let i = 0; i < NUM_OF_FOODS_EACH_COURT["COURT" + k]; i++) {
+//         createFoodinCourt(courtFoodList, foodInfo[k]);
+//     }
+//     container.append(courtFoodList);
+// }
+
+axios({
+    method: 'GET',
+    url: 'https://matb-app.herokuapp.com/api/vendor'
+})
+    .then((res) => {
+        console.log(res.status);
+        res.data.forEach(data => {
+            console.log(data.name);
+            data.foodList.forEach(foodList => {
+                createFoodinCourt(container, foodList);
+            });
+        });
+    })
+    .catch(err => console.error(err));
+
+function formatNumber(num) {
+    if (num == 0) {
+        return 0;
+    }     
+    else {
+        return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,');
     }
-    container.append(courtFoodList);
 }
